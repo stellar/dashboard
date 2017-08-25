42f50e3099f4b62400d4046a3cf2643a00c5bdcb
Bartek Nowotarski <bartek@nowotarski.info>
2017-08-26 01:35:36 +0200

    Start monitoring core nodes

--- app.js
+++ app.js
@@ -3,12 +3,18 @@
 require("babel-register")({
   "presets": ["es2015"]
 });
+var _ = require('lodash');
 var express = require('express');
 var stellarSdk = require('stellar-sdk');
 var moment = require('moment');
+var net = require('net');
 var redis = require("redis"),
     redisClient = redis.createClient(process.env.REDIS_URL);
 var lumens = require("./common/lumens.js");
+var nodes = require("./common/nodes.js");
+
+const NODE_ERROR = -1;
+const NODE_TIMEOUT = -2;
 
 var app = express();
 app.set('port', (process.env.PORT || 5000));
@@ -98,6 +104,63 @@ function updateApiLumens() {
 
 setInterval(updateApiLumens, 10*60*1000);
 updateApiLumens();
+
+function checkNode(node) {
+  return new Promise((resolve, reject) => {
+    let client = new net.Socket();
+    let start = moment();
+    client.setTimeout(10*1000);
+
+    client.connect(node.port, node.host, function() {
+      client.end();
+      resolve({id: node.id, connectedIn: moment().diff(start)});
+    });
+
+    client.on('error', () => {
+      client.end();
+      resolve({id: node.id, connectedIn: NODE_ERROR});
+    });
+
+    client.on('timeout', () => {
+      client.end();
+      resolve({id: node.id, connectedIn: NODE_TIMEOUT});
+    });
+  });
+}
+
+function checkNodes() {
+  let date = moment();
+
+  // Check uptime every 5 minutes
+  if (date.minutes() % 5 != 0) {
+    return;
+  }
+
+  Promise.all(_.map(nodes, checkNode))
+    .then(results => {
+      let multi = redisClient.multi();
+      var key = "nodes_"+date.format("YYYY-MM-DD_HH:mm")
+
+      for (let result of results) {
+        multi.hset(key, result.id, result.connectedIn);
+      }
+
+      // Expire the key after 32 days
+      multi.expire(key, 60*60*24*32);
+
+      multi.exec(function (err, res) {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        console.log("Added nodes uptime stats: "+key+" "+res);
+      });
+
+    });
+}
+
+setInterval(checkNodes, 60*1000);
+checkNodes();
 
 // Stream ledgers - get last paging_token
 redisClient.get('paging_token', function(err, pagingToken) {
---
+++ common/nodes.js
@@ -0,0 +1,44 @@
+module.exports = [
+  {
+    id: "sdf1",
+    name: "SDF validator 1",
+    host: "core-live-a.stellar.org",
+    port: 11625,
+    publicKey: "GCGB2S2KGYARPVIA37HYZXVRM2YZUEXA6S33ZU5BUDC6THSB62LZSTYH"
+  },
+  {
+    id: "sdf2",
+    name: "SDF validator 2",
+    host: "core-live-b.stellar.org",
+    port: 11625,
+    publicKey: "GCM6QMP3DLRPTAZW2UZPCPX2LF3SXWXKPMP3GKFZBDSF3QZGV2G5QSTK"
+  },
+  {
+    id: "sdf3",
+    name: "SDF validator 3",
+    host: "core-live-c.stellar.org",
+    port: 11625,
+    publicKey: "GABMKJM6I25XI4K7U6XWMULOUQIQ27BCTMLS6BYYSOWKTBUXVRJSXHYQ"
+  },
+  {
+    id: "eno",
+    name: "Eno",
+    host: "stellar.256kw.com",
+    port: 11625,
+    publicKey: "GAOO3LWBC4XF6VWRP5ESJ6IBHAISVJMSBTALHOQM2EZG7Q477UWA6L7U"
+  },
+  {
+    id: "tempo",
+    name: "Tempo",
+    host: "stellar1.tempo.eu.com",
+    port: 11625,
+    publicKey: "GCJCSMSPIWKKPR7WEPIQG63PDF7JGGEENRC33OKVBSPUDIRL6ZZ5M7OO"
+  },
+  {
+    id: "satoshipay",
+    name: "satoshipay.io",
+    host: "stellar.satoshipay.io",
+    port: 11625,
+    publicKey: "GC5SXLNAM3C4NMGK2PXK4R34B5GNZ47FYQ24ZIBFDFOCU6D4KBN4POAE"
+  }
+];
