d6f67f85a9c5a35e7fb392f88a9995f938f52d73
Bartek Nowotarski <bartek@nowotarski.info>
2018-12-12 12:54:43 +0100

    Remove redis dependency (#67)

--- backend/ledgers.js
+++ backend/ledgers.js
@@ -1,68 +1,51 @@
 import stellarSdk from 'stellar-sdk';
 import moment from 'moment';
-import {redisClient} from './redis.js';
+import _ from 'lodash';
+import * as postgres from './postgres.js';
+
+let cachedData;
 
 export const handler = function(req, res) {
-  var day = moment();
-  var response = [];
-
-  var multi = redisClient.multi();
-
-  var days = 30;
-
-  for (var i = 0; i < days; i++) {
-    response.push({date: day.format("DD-MM")});
-    multi.hmget('ledger_public'+day.format("YYYY-MM-DD"), 'transaction_count', 'operation_count');
-    day = day.subtract(1, 'days')
-  }
-
-  multi.exec(function (err, redisRes) {
-    if (err) {
-      res.sendStatus(500);
-      console.error(err);
-      res.error("Error");
-      return;
-    }
-
-    for (var i = 0; i < days; i++) {
-      response[i].transaction_count = parseInt(redisRes[i][0]);
-      response[i].operation_count = parseInt(redisRes[i][1]);
-    }
-
-    res.send(response);
-  });
+  res.send(cachedData);
 }
 
-// Stream ledgers - get last paging_token
-redisClient.get('paging_token', function(err, pagingToken) {
-  if (err) {
-    console.error(err);
-    return
-  }
+function updateResults() {
+  let query =
+    `select
+      to_char(closed_at, 'DD-MM') as date,
+      sum(transaction_count) as transaction_count,
+      sum(operation_count) as operation_count
+    from ledger_stats
+    where closed_at >= current_date - interval '30' day
+    group by to_char(closed_at, 'DD-MM')
+    order by 1 desc`;
 
-  if (!pagingToken) {
-    pagingToken = 'now';
-  }
+  postgres.sequelize.query(query, {type: postgres.sequelize.QueryTypes.SELECT})
+    .then(results => cachedData = results);
+}
 
-  var horizon = new stellarSdk.Server('https://horizon.stellar.org');
-  horizon.ledgers().cursor(pagingToken).stream({
-    onmessage: function(ledger) {
-      var date = moment(ledger.closed_at).format("YYYY-MM-DD");
-      var key = "ledger_public"+date;
+// Wait for schema sync
+postgres.sequelize.addHook('afterBulkSync', () => {
+  setInterval(updateResults, 60*1000);
+  updateResults();
 
-      var multi = redisClient.multi();
-      multi.hincrby(key, "transaction_count", ledger.transaction_count);
-      multi.hincrby(key, "operation_count", ledger.operation_count);
-      // Expire the key after 32 days
-      multi.expire(key, 60*60*24*32);
-      multi.set("paging_token", ledger.paging_token);
-      multi.exec(function (err, res) {
-        if (err) {
-          console.error(err);
-          return;
-        }
-        console.log("Added Ledger:"+ledger.sequence+" "+ledger.closed_at+" "+res);
-      });
+  // Stream ledgers - get last paging_token/
+  postgres.LedgerStats.findOne({order: [['sequence', 'DESC']]}).then(lastLedger => {
+    let pagingToken;
+    if (!lastLedger) {
+      pagingToken = 'now';
+    } else {
+      pagingToken = lastLedger.paging_token;
     }
+
+    var horizon = new stellarSdk.Server('https://horizon.stellar.org');
+    horizon.ledgers().cursor(pagingToken).stream({
+      onmessage: ledger => {
+        postgres.LedgerStats.create(_.pick(ledger, ['sequence', 'closed_at', 'paging_token', 'transaction_count', 'operation_count']))
+          .then(() => {
+            console.log("Added Ledger:"+ledger.sequence+" "+ledger.closed_at);
+          });
+      }
+    });
   });
 });
