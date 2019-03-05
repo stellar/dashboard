78cd946c9c8e7fcab7aee29a0d7ee2578b4a056e
Johnny Goodnow <jgoodnow@fastmail.com>
2019-03-05 04:11:35 -0500

    Change testnet reset to April 2019 date (#86)

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -112,7 +112,7 @@ export default class App extends React.Component {
 
         <Panel className="mui--bg-accent-light">
           <div className="mui--text-subhead mui--text-light">
-            The test network will be reset on February 27th, 2019 at 0900 UTC. Please see our <a href="https://www.stellar.org/developers/guides/concepts/test-net.html#best-practices-for-using-testnet">testnet best practices</a> for more information.
+            The test network will be reset on April 24th, 2019 at 0900 UTC. Please see our <a href="https://www.stellar.org/developers/guides/concepts/test-net.html#best-practices-for-using-testnet">testnet best practices</a> for more information.
           </div>
         </Panel>
 
