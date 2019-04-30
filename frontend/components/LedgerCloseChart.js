39dc77812a2c638faeeafda132108e8ae6b9df39
Dennis Dang <dangggdennis@gmail.com>
2019-04-30 01:28:21 -0700

    Implement last ledgers with Nivo bar

--- frontend/components/LedgerCloseChart.js
+++ frontend/components/LedgerCloseChart.js
@@ -3,6 +3,7 @@ import Panel from 'muicss/lib/react/panel';
 import axios from 'axios';
 import {scale} from 'd3';
 import BarChart from 'react-d3-components/lib/BarChart';
+import { ResponsiveBar } from '@nivo/bar'
 import each from 'lodash/each';
 import clone from 'lodash/clone';
 
@@ -48,7 +49,15 @@ export default class LedgerChartClose extends React.Component {
           data[0].values.unshift({x: ledger.sequence.toString(), y: diff});
           this.lastLedgerClosedAt = closedAt;
         });
-        this.setState({loading: false, data});
+
+        // Reduce a separate data structure for the nivo bar chart
+        const nivoData = this.nivoReducer(response.data._embedded.records)
+        this.setState({loading: false, data, nivoData });
+
+        // TODO: Remove logs
+        console.log('records', response.data._embedded.records)
+        console.log({nivoData})
+
         // Start listening to events
         this.props.emitter.addListener(this.props.newLedgerEventName, this.onNewLedger.bind(this));
       });
@@ -69,6 +78,25 @@ export default class LedgerChartClose extends React.Component {
     this.frontLedgerClosedAt = closedAt;
   }
 
+  nivoReducer(ledgers) {
+    // TODO: Clean up duplicate logic from above
+    this.lastLedgerClosedAt = null;
+    return ledgers.map(ledger => {
+      const closedAt = new Date(ledger.closed_at);
+      if (this.lastLedgerClosedAt == null) {
+        this.lastLedgerClosedAt = closedAt;
+        this.frontLedgerClosedAt = closedAt; // used in onNewLedger
+        return;
+      }
+      const diff = (this.lastLedgerClosedAt - closedAt)/1000;
+      this.lastLedgerClosedAt = closedAt;
+      return {
+        id: ledger.sequence,
+        closedAt: diff,
+      }
+    }).filter(ledger => ledger) // Removes random null values
+  }
+
   render() {
     return (
       <div ref={(el) => { this.panel = el; }}>
@@ -85,10 +113,105 @@ export default class LedgerChartClose extends React.Component {
               width={this.state.chartWidth}
               colorScale={this.colorScale}
               height={this.state.chartHeigth}
-              margin={{top: 10, bottom: 8, left: 50, right: 10}}/>
+              margin={{top: 10, bottom: 8, left: 50, right: 10}}
+              onMouseEnter={(e, data) => console.log("ledger: ", data)}
+              onMouseLeave={() => ({})}
+              />
+          }
+        </Panel>
+        <Panel style={{ height: '200px'}}> 
+          {
+            this.state.loading
+              ? 'Loading...'
+              : <React.Fragment>
+                  <div className="widget-name">
+                    Last {this.props.limit} ledgers close times: {this.props.network} (Made With Nivo Bar Chart)
+                    <a href={this.url} target="_blank" className="api-link">API</a>
+                  </div>
+                  <LedgerChartNivoBar data={this.state.nivoData} />
+                </React.Fragment>
           }
         </Panel>
       </div>
     );
   }
 }
+
+// make sure parent container have a defined height when using
+// responsive component, otherwise height will be 0 and
+// no chart will be rendered.
+// website examples showcase many properties,
+// you'll often use just a few of them.
+const LedgerChartNivoBar = ({ data }) => (
+  <ResponsiveBar
+      data={data}
+      keys={[
+          // "id",
+          "closedAt"
+      ]}
+      indexBy="id"
+      margin={{
+          "bottom": 20,
+          "left": 60,
+          "top": 20,
+      }}
+      padding={0.3}
+      colors={{
+          "scheme": "nivo"
+      }}
+      defs={[
+          {
+              "id": "dots",
+              "type": "patternDots",
+              "background": "inherit",
+              "color": "#38bcb2",
+              "size": 4,
+              "padding": 1,
+              "stagger": true
+          },
+          {
+              "id": "lines",
+              "type": "patternLines",
+              "background": "inherit",
+              "color": "#eed312",
+              "rotation": -45,
+              "lineWidth": 6,
+              "spacing": 10
+          }
+      ]}
+      borderColor={{
+          "from": "color",
+          "modifiers": [
+              [
+                  "darker",
+                  1.6
+              ]
+          ]
+      }}
+      axisTop={null}
+      axisRight={null}
+      axisBottom={null}
+      axisLeft={{
+          "tickSize": 5,
+          "tickPadding": 5,
+          "tickRotation": 0,
+          "legendPosition": "middle",
+          "legendOffset": -40
+      }}
+      labelSkipWidth={12}
+      labelSkipHeight={12}
+      labelTextColor={{
+          "from": "color",
+          "modifiers": [
+              [
+                  "darker",
+                  1.6
+              ]
+          ]
+      }}
+      legends={[]}
+      animate={true}
+      motionStiffness={90}
+      motionDamping={15}
+  />
+)
