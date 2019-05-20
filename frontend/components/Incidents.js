cef0e1178b646b954dc9d29d593b26db234712f5
Bartek Nowotarski <bartek@nowotarski.info>
2019-05-20 14:14:58 +0200

    Integrate with Status Page

---
+++ frontend/components/Incidents.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import Panel from 'muicss/lib/react/panel';
+import axios from 'axios';
+import moment from 'moment';
+import clone from 'lodash/clone';
+import each from 'lodash/each';
+import defaults from 'lodash/defaults';
+import AccountBadge from './AccountBadge';
+import AssetLink from './AssetLink';
+import BigNumber from 'bignumber.js';
+import {ago} from '../common/time';
+
+export default class Incidents extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {loading: true};
+  }
+
+  getIncidents() {
+    if (this.loading) {
+      return;
+    }
+    this.loading = true;
+
+    axios.get("https://9sl3dhr1twv1.statuspage.io/api/v2/incidents.json?limit=10")
+      .then(response => {
+        this.setState({loading: false, incidents: response.data.incidents});
+        this.loading = false;
+      });
+  }
+
+  componentDidMount() {
+    this.getIncidents();
+    this.timerID = setInterval(() => this.getIncidents(), 60*1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  render() {
+    return (
+      <Panel>
+        <div className="widget-name">
+          Incidents
+        </div>
+        {
+            this.state.loading ?
+              <span>Loading...</span>
+            :
+            <ul className="incidents">
+            {this.state.incidents.map(m => {
+              return <li key={m.id}><a href={"https://stellarorg.statuspage.io/incidents/"+m.id}>{m.name}</a> ({moment(m.started_at).fromNow()})</li>
+            })}
+            </ul>
+          }
+      </Panel>
+    );
+  }
+}
