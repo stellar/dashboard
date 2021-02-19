4e90d1052a58b852e91e1643553fe2db2a15d341
Iveta <quietbits@gmail.com>
2021-02-19 14:44:52 -0500

    Allow only safe HTML elements

--- frontend/utilities/sanitizeHtml.js
+++ frontend/utilities/sanitizeHtml.js
@@ -2,7 +2,7 @@ import parse from "html-react-parser";
 import DOMPurify from "dompurify";
 
 function sanitizeHtml(html) {
-  return parse(DOMPurify.sanitize(html));
+  return parse(DOMPurify.sanitize(html, { USE_PROFILES: { html: true } }));
 }
 
 export default sanitizeHtml;
