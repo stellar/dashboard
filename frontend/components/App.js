e0d884e0e6c4bfd13e80a7b6201caf2f997c2367
Alec Charbonneau <aleccharb21@gmail.com>
2021-12-13 22:55:48 -0800

    fixed *fingers crossed*

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -26,30 +26,6 @@ import sanitizeHtml from "../utilities/sanitizeHtml.js";
 const horizonLive = "https://horizon.stellar.org";
 const horizonTest = "https://horizon-testnet.stellar.org";
 
-// ALEC TODO - change back
-// export default class App extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.panel = null;
-//   }
-
-//   componentDidMount() {
-//     console.log(this.panel);
-//   }
-
-//   render() {
-//     return (
-//       <div
-//         ref={(el) => {
-//           this.panel = el;
-//         }}
-//       >
-//         Testing panel
-//       </div>
-//     );
-//   }
-// }
-
 export default class App extends React.Component {
   constructor(props) {
     super(props);
@@ -176,10 +152,76 @@ export default class App extends React.Component {
     return false;
   }
 
-  // ALEC TODO - change back
   render() {
     return (
       <div id="main" className={this.state.forceTheme ? "force" : null}>
+        <AppBar
+          forceTheme={this.state.forceTheme}
+          turnOffForceTheme={this.turnOffForceTheme.bind(this)}
+        />
+
+        {/* Incidents */
+        this.state.statusPage
+          ? this.state.statusPage.incidents.map((m) => {
+              return (
+                <Panel key={m.id} className="mui--bg-accent">
+                  <div className="mui--text-subhead mui--text-light">
+                    <a href={"https://status.stellar.org/incidents/" + m.id}>
+                      <strong>{m.name}</strong>
+                    </a>{" "}
+                    (started: {moment(m.started_at).fromNow()}
+                    {m.incident_updates.length > 0
+                      ? ", last update: " +
+                        moment(m.incident_updates[0].created_at).fromNow()
+                      : null}
+                    )<br />
+                    <small>
+                      Affected: {m.components.map((c) => c.name).join(", ")}
+                    </small>
+                    <br />
+                    {m.incident_updates.length > 0 ? (
+                      <span>{sanitizeHtml(m.incident_updates[0].body)}</span>
+                    ) : null}
+                  </div>
+                </Panel>
+              );
+            })
+          : null}
+        {/* Scheduled maintenances */
+        this.state.statusPage &&
+        this.state.statusPage.scheduled_maintenances.length ? (
+          <ScheduledMaintenance
+            scheduledMaintenances={this.state.statusPage.scheduled_maintenances}
+          />
+        ) : null}
+        {this.chrome57 ? (
+          <Panel>
+            <div className="mui--text-subhead mui--text-dark-secondary">
+              You are using Chrome 57. There is a{" "}
+              <a
+                href="https://bugs.chromium.org/p/chromium/issues/detail?id=707544"
+                target="_blank"
+              >
+                known bug
+              </a>{" "}
+              that makes the Dashboard app consume extensive amounts of memory.
+              Please switch to any other browser or wait for a fix by a Chromium
+              team.
+            </div>
+          </Panel>
+        ) : null}
+        {this.state.sleeping ? (
+          <Panel>
+            <div className="mui--text-subhead mui--text-accent">
+              System sleep detected. Waiting for internet connection...
+            </div>
+          </Panel>
+        ) : null}
+        {this.state.forceTheme && this.state.may4 ? (
+          <h1 className="may4">
+            May the 4<sup>th</sup> be with you!
+          </h1>
+        ) : null}
         <div id="main" className="mui-container-fluid">
           <section>
             <h1>Live network status</h1>
@@ -226,211 +268,90 @@ export default class App extends React.Component {
               </div>
             </div>
           </section>
+
+          <section>
+            <h1>LUMEN SUPPLY</h1>
+            <div className="mui-col-md-4">
+              <TotalCoins />
+            </div>
+
+            <div className="mui-col-md-4">
+              <LumensNonCirculating />
+            </div>
+
+            <div className="mui-col-md-4">
+              <LumensCirculating />
+            </div>
+            <h2>
+              <a
+                href="https://www.stellar.org/developers/guides/lumen-supply-metrics.html"
+                target="_blank"
+              >
+                Lumen Supply Metrics
+              </a>
+            </h2>
+          </section>
+
+          <section>
+            <h1>Network Nodes</h1>
+            <h2>
+              View network nodes on Stellarbeat and visualize consensus.
+              <br />
+              <a href="https://stellarbeat.io" target="_blank">
+                Explore Nodes
+              </a>
+            </h2>
+          </section>
+
+          <section>
+            <h1>Test network status</h1>
+            <div className="mui-col-md-4">
+              <NetworkStatus
+                network="Test network"
+                horizonURL={horizonTest}
+                newLedgerEventName={TEST_NEW_LEDGER}
+                emitter={this.emitter}
+              />
+              <RecentOperations
+                limit="20"
+                label="Test network"
+                horizonURL={horizonTest}
+                emitter={this.emitter}
+              />
+            </div>
+            <div className="mui-col-md-8">
+              <LedgerCloseChart
+                network="Test network"
+                horizonURL={horizonTest}
+                limit="100"
+                newLedgerEventName={TEST_NEW_LEDGER}
+                emitter={this.emitter}
+              />
+              <TransactionsChart
+                network="Test network"
+                horizonURL={horizonTest}
+                limit="100"
+                newLedgerEventName={TEST_NEW_LEDGER}
+                emitter={this.emitter}
+              />
+              <FailedTransactionsChart
+                network="Test network"
+                horizonURL={horizonTest}
+                limit="100"
+                newLedgerEventName={TEST_NEW_LEDGER}
+                emitter={this.emitter}
+              />
+            </div>
+            <div className="mui-col-md-4">
+              <AccountBalance
+                horizonURL={horizonTest}
+                name="Friendbot"
+                id="GAIH3ULLFQ4DGSECF2AR555KZ4KNDGEKN4AFI4SU2M7B43MGK3QJZNSR"
+              />
+            </div>
+          </section>
         </div>
       </div>
     );
   }
-
-  // render() {
-  //   return (
-  //     <div id="main" className={this.state.forceTheme ? "force" : null}>
-  //       <AppBar
-  //         forceTheme={this.state.forceTheme}
-  //         turnOffForceTheme={this.turnOffForceTheme.bind(this)}
-  //       />
-
-  //       {/* Incidents */
-  //       this.state.statusPage
-  //         ? this.state.statusPage.incidents.map((m) => {
-  //             return (
-  //               <Panel key={m.id} className="mui--bg-accent">
-  //                 <div className="mui--text-subhead mui--text-light">
-  //                   <a href={"https://status.stellar.org/incidents/" + m.id}>
-  //                     <strong>{m.name}</strong>
-  //                   </a>{" "}
-  //                   (started: {moment(m.started_at).fromNow()}
-  //                   {m.incident_updates.length > 0
-  //                     ? ", last update: " +
-  //                       moment(m.incident_updates[0].created_at).fromNow()
-  //                     : null}
-  //                   )<br />
-  //                   <small>
-  //                     Affected: {m.components.map((c) => c.name).join(", ")}
-  //                   </small>
-  //                   <br />
-  //                   {m.incident_updates.length > 0 ? (
-  //                     <span>{sanitizeHtml(m.incident_updates[0].body)}</span>
-  //                   ) : null}
-  //                 </div>
-  //               </Panel>
-  //             );
-  //           })
-  //         : null}
-  //       {/* Scheduled maintenances */
-  //       this.state.statusPage &&
-  //       this.state.statusPage.scheduled_maintenances.length ? (
-  //         <ScheduledMaintenance
-  //           scheduledMaintenances={this.state.statusPage.scheduled_maintenances}
-  //         />
-  //       ) : null}
-  //       {this.chrome57 ? (
-  //         <Panel>
-  //           <div className="mui--text-subhead mui--text-dark-secondary">
-  //             You are using Chrome 57. There is a{" "}
-  //             <a
-  //               href="https://bugs.chromium.org/p/chromium/issues/detail?id=707544"
-  //               target="_blank"
-  //             >
-  //               known bug
-  //             </a>{" "}
-  //             that makes the Dashboard app consume extensive amounts of memory.
-  //             Please switch to any other browser or wait for a fix by a Chromium
-  //             team.
-  //           </div>
-  //         </Panel>
-  //       ) : null}
-  //       {this.state.sleeping ? (
-  //         <Panel>
-  //           <div className="mui--text-subhead mui--text-accent">
-  //             System sleep detected. Waiting for internet connection...
-  //           </div>
-  //         </Panel>
-  //       ) : null}
-  //       {this.state.forceTheme && this.state.may4 ? (
-  //         <h1 className="may4">
-  //           May the 4<sup>th</sup> be with you!
-  //         </h1>
-  //       ) : null}
-  //       <div id="main" className="mui-container-fluid">
-  //         <section>
-  //           <h1>Live network status</h1>
-  //           <div className="row">
-  //             <div className="mui-col-md-4">
-  //               <NetworkStatus
-  //                 network="Live network"
-  //                 horizonURL={horizonLive}
-  //                 newLedgerEventName={LIVE_NEW_LEDGER}
-  //                 emitter={this.emitter}
-  //               />
-  //               <Incidents />
-  //               <FeeStats horizonURL={horizonLive} />
-  //               <RecentOperations
-  //                 limit="20"
-  //                 label="Live network"
-  //                 horizonURL={horizonLive}
-  //                 emitter={this.emitter}
-  //               />
-  //             </div>
-  //             <div className="mui-col-md-8">
-  //               <LedgerCloseChart
-  //                 network="Live network"
-  //                 horizonURL={horizonLive}
-  //                 limit="100"
-  //                 newLedgerEventName={LIVE_NEW_LEDGER}
-  //                 emitter={this.emitter}
-  //               />
-  //               <TransactionsChart
-  //                 network="Live network"
-  //                 horizonURL={horizonLive}
-  //                 limit="100"
-  //                 newLedgerEventName={LIVE_NEW_LEDGER}
-  //                 emitter={this.emitter}
-  //               />
-  //               <FailedTransactionsChart
-  //                 network="Live network"
-  //                 horizonURL={horizonLive}
-  //                 limit="100"
-  //                 newLedgerEventName={LIVE_NEW_LEDGER}
-  //                 emitter={this.emitter}
-  //               />
-  //               <PublicNetworkLedgersHistoryChart />
-  //             </div>
-  //           </div>
-  //         </section>
-
-  //         <section>
-  //           <h1>LUMEN SUPPLY</h1>
-  //           <div className="mui-col-md-4">
-  //             <TotalCoins />
-  //           </div>
-
-  //           <div className="mui-col-md-4">
-  //             <LumensNonCirculating />
-  //           </div>
-
-  //           <div className="mui-col-md-4">
-  //             <LumensCirculating />
-  //           </div>
-  //           <h2>
-  //             <a
-  //               href="https://www.stellar.org/developers/guides/lumen-supply-metrics.html"
-  //               target="_blank"
-  //             >
-  //               Lumen Supply Metrics
-  //             </a>
-  //           </h2>
-  //         </section>
-
-  //         <section>
-  //           <h1>Network Nodes</h1>
-  //           <h2>
-  //             View network nodes on Stellarbeat and visualize consensus.
-  //             <br />
-  //             <a href="https://stellarbeat.io" target="_blank">
-  //               Explore Nodes
-  //             </a>
-  //           </h2>
-  //         </section>
-
-  //         <section>
-  //           <h1>Test network status</h1>
-  //           <div className="mui-col-md-4">
-  //             <NetworkStatus
-  //               network="Test network"
-  //               horizonURL={horizonTest}
-  //               newLedgerEventName={TEST_NEW_LEDGER}
-  //               emitter={this.emitter}
-  //             />
-  //             <RecentOperations
-  //               limit="20"
-  //               label="Test network"
-  //               horizonURL={horizonTest}
-  //               emitter={this.emitter}
-  //             />
-  //           </div>
-  //           <div className="mui-col-md-8">
-  //             <LedgerCloseChart
-  //               network="Test network"
-  //               horizonURL={horizonTest}
-  //               limit="100"
-  //               newLedgerEventName={TEST_NEW_LEDGER}
-  //               emitter={this.emitter}
-  //             />
-  //             <TransactionsChart
-  //               network="Test network"
-  //               horizonURL={horizonTest}
-  //               limit="100"
-  //               newLedgerEventName={TEST_NEW_LEDGER}
-  //               emitter={this.emitter}
-  //             />
-  //             <FailedTransactionsChart
-  //               network="Test network"
-  //               horizonURL={horizonTest}
-  //               limit="100"
-  //               newLedgerEventName={TEST_NEW_LEDGER}
-  //               emitter={this.emitter}
-  //             />
-  //           </div>
-  //           <div className="mui-col-md-4">
-  //             <AccountBalance
-  //               horizonURL={horizonTest}
-  //               name="Friendbot"
-  //               id="GAIH3ULLFQ4DGSECF2AR555KZ4KNDGEKN4AFI4SU2M7B43MGK3QJZNSR"
-  //             />
-  //           </div>
-  //         </section>
-  //       </div>
-  //     </div>
-  //   );
-  // }
 }
