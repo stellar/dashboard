5a1950c5a947ad5e06339bd959cc570fcbfe8edb
Alec Charbonneau <aleccharb21@gmail.com>
2021-12-16 16:52:02 -0800

    remove compiled files

--- backend/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-require("babel-register")({
-  presets: ["es2015"],
-});
-var updateLumensCache = require("./routes").updateLumensCache;
-setInterval(updateLumensCache, 10 * 60 * 1000);
-updateLumensCache();
-//# sourceMappingURL=app.js.map
