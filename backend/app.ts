f99ed12593ab0038e46b4f941e5fe0df0253df33
Alec Charbonneau <aleccharb21@gmail.com>
2021-12-21 16:51:44 -0800

    fix docker run error

--- backend/app.ts
+++ backend/app.ts
@@ -1,6 +1,9 @@
 require("babel-register")({
   presets: ["es2015"],
 });
+// need to manually import regeneratorRuntime for babel w/ async
+// https://github.com/babel/babel/issues/9849#issuecomment-487040428
+require("regenerator-runtime/runtime");
 
 // Run backend with interval cache updates.
 const { updateLumensCache } = require("./routes");
