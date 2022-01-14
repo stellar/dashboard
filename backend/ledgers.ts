b2f0676c953a5057ac9163d2660027b5aa6e6e6b
Alec Charbonneau <aleccharb21@gmail.com>
2022-01-14 09:28:24 -0800

    backend: remove postgres, use redis for api/ledgers data (#209)
    * remove pq, first cut
    
    * add unit tests, finish todos
    
    * export
    
    * only update with UPDATE_DATA flag
    
    * remove postgres file, sort cached ledgers
    
    * fix flag
    
    * separate and await the different cache updates
    
    * delete postgres deps, cleanup
    
    * add formatDate helper
    
    * use CURSOR_NOW, missing REDIS_PAGING_TOKEN_KEY
    
    * replace let with const
    
    * the future is now
    
    * change test const names
    
    * use LedgerStats
    
    * test cleanup

--- backend/ledgers.ts
+++ backend/ledgers.ts
@@ -1,22 +1,21 @@
 import stellarSdk from "stellar-sdk";
-import { map, pick } from "lodash";
-import { QueryTypes } from "sequelize";
+import { findIndex } from "lodash";
 import { Response, NextFunction } from "express";
-import * as postgres from "./postgres";
 
 import { redisClient, getOrThrow } from "./redis";
 
-interface Ledger {
+interface LedgerStat {
   date: string;
   transaction_count: number;
   operation_count: number;
 }
 
-interface LedgerSql {
-  date: string;
-  transaction_count: string;
-  operation_count: string;
-}
+// TODO - use any until https://github.com/stellar/js-stellar-sdk/issues/731 resolved
+export type LedgerRecord = any;
+
+const REDIS_LEDGER_KEY = "ledgers";
+const REDIS_PAGING_TOKEN_KEY = "paging_token";
+const CURSOR_NOW = "now";
 
 export const handler = async function (
   _: any,
@@ -24,82 +23,120 @@ export const handler = async function (
   next: NextFunction,
 ) {
   try {
-    const cachedData = await getOrThrow(redisClient, "ledgers");
-    const ledgers: Array<Ledger> = JSON.parse(cachedData as string);
+    const cachedData = await getOrThrow(redisClient, REDIS_LEDGER_KEY);
+    const ledgers: Array<LedgerStat> = JSON.parse(cachedData as string);
     res.json(ledgers);
   } catch (e) {
     return next(e);
   }
 };
 
-function updateResults() {
-  let query = `select
-      to_char(closed_at, 'YYYY-MM-DD') as date,
-      sum(transaction_count) as transaction_count,
-      sum(operation_count) as operation_count
-    from ledger_stats
-    where closed_at >= current_date - interval '29' day
-    group by to_char(closed_at, 'YYYY-MM-DD')
-    order by 1 desc`;
+export async function updateLedgers() {
+  let pagingToken = await redisClient.get(REDIS_PAGING_TOKEN_KEY);
+  if (pagingToken == null || pagingToken == "") {
+    pagingToken = CURSOR_NOW;
+  }
 
-  postgres.sequelize
-    .query(query, { type: QueryTypes.SELECT })
-    .then((results: Array<LedgerSql>) => {
-      const cachedData: Array<Ledger> = map(results, convertFields);
-      redisClient.set("ledgers", JSON.stringify(cachedData));
+  await catchup(REDIS_LEDGER_KEY, pagingToken, REDIS_PAGING_TOKEN_KEY, 0);
+
+  var horizon = new stellarSdk.Server("https://horizon.stellar.org");
+  horizon
+    .ledgers()
+    .cursor(CURSOR_NOW)
+    .limit(200)
+    .stream({
+      onmessage: async (ledger: LedgerRecord) => {
+        await updateCache([ledger], REDIS_LEDGER_KEY, REDIS_PAGING_TOKEN_KEY);
+      },
     });
 }
 
-function convertFields(ledger: LedgerSql): Ledger {
-  return {
-    // Remove year from date
-    date: ledger["date"].substring(5),
-    transaction_count: parseInt(ledger.transaction_count),
-    operation_count: parseInt(ledger.operation_count),
-  };
+export async function catchup(
+  ledgersKey: string,
+  pagingToken: string,
+  pagingTokenKey: string,
+  limit: number, // if 0, catchup until now
+) {
+  const horizon = new stellarSdk.Server("https://horizon.stellar.org");
+  let ledgers = [];
+  let total = 0;
+
+  while (true) {
+    const resp = await horizon.ledgers().cursor(pagingToken).limit(200).call();
+    ledgers = resp.records;
+    total += resp.records.length;
+    if (ledgers.length == 0 || (limit && total > limit)) {
+      break;
+    }
+
+    pagingToken = ledgers[ledgers.length - 1].paging_token;
+    await updateCache(ledgers, ledgersKey, pagingTokenKey);
+  }
 }
 
-// Wait for schema sync
-postgres.sequelize.addHook("afterBulkSync", () => {
-  setInterval(updateResults, 60 * 1000);
-  updateResults();
-
-  if (process.env.UPDATE_DATA == "true") {
-    // Stream ledgers - get last paging_token/
-    postgres.LedgerStats.findOne({ order: [["sequence", "DESC"]] }).then(
-      (lastLedger: postgres.LedgerStats) => {
-        let pagingToken;
-        if (!lastLedger) {
-          pagingToken = "now";
-        } else {
-          pagingToken = lastLedger.paging_token;
-        }
-
-        var horizon = new stellarSdk.Server("https://horizon.stellar.org");
-        horizon
-          .ledgers()
-          .cursor(pagingToken)
-          .limit(200)
-          .stream({
-            // TODO - use type any until https://github.com/stellar/js-stellar-sdk/issues/731 resolved
-            onmessage: (ledger: any) => {
-              let newLedger: any = pick(ledger, [
-                "sequence",
-                "closed_at",
-                "paging_token",
-                "operation_count",
-              ]);
-              newLedger["transaction_count"] =
-                ledger.successful_transaction_count +
-                ledger.failed_transaction_count;
-              postgres.LedgerStats.create(newLedger).then(() => {
-                console.log(
-                  "Added Ledger:" + ledger.sequence + " " + ledger.closed_at,
-                );
-              });
-            },
-          });
-      },
-    );
+export async function updateCache(
+  ledgers: Array<LedgerRecord>,
+  ledgersKey: string,
+  pagingTokenKey: string,
+) {
+  if (!ledgers.length) {
+    console.log("no ledgers to update");
+    return;
   }
-});
+  const json = (await redisClient.get(ledgersKey)) || "[]";
+  const cachedStats: Array<LedgerStat> = JSON.parse(json);
+  let pagingToken = "";
+
+  ledgers.forEach((ledger: LedgerRecord) => {
+    const date = formatDate(ledger.closed_at);
+    const index = findIndex(cachedStats, { date });
+    if (index == -1) {
+      cachedStats.push({
+        date: date,
+        transaction_count:
+          ledger.successful_transaction_count + ledger.failed_transaction_count,
+        operation_count: ledger.operation_count,
+      });
+    } else {
+      cachedStats.splice(index, 1, {
+        date: date,
+        transaction_count:
+          cachedStats[index].transaction_count +
+          ledger.successful_transaction_count +
+          ledger.failed_transaction_count,
+        operation_count:
+          cachedStats[index].operation_count + ledger.operation_count,
+      });
+    }
+    pagingToken = ledger.paging_token;
+  });
+  cachedStats.sort(dateSorter);
+
+  // only store latest 30 days
+  await redisClient.set(ledgersKey, JSON.stringify(cachedStats.slice(0, 30)));
+  await redisClient.set(pagingTokenKey, pagingToken);
+
+  console.log("ledgers updated to:", ledgers[ledgers.length - 1].closed_at);
+}
+
+function dateSorter(a: LedgerStat, b: LedgerStat) {
+  let dateA = new Date(a.date);
+  let dateB = new Date(b.date);
+
+  if (dateA.getMonth() == 11) {
+    dateA.setFullYear(dateA.getFullYear() - 1);
+  }
+  if (dateB.getMonth() == 11) {
+    dateB.setFullYear(dateB.getFullYear() - 1);
+  }
+
+  return dateB.getTime() - dateA.getTime();
+}
+
+// MM-DD
+function formatDate(s: string) {
+  const d = new Date(s);
+  return `${("0" + (d.getUTCMonth() + 1)).slice(-2)}-${(
+    "0" + d.getUTCDate()
+  ).slice(-2)}`;
+}
--- test/tests/unit/backend.ts
+++ test/tests/unit/backend.ts
@@ -1,7 +1,11 @@
 import chai from "chai";
 import { redisClient } from "../../../backend/redis";
+import { updateCache, catchup, LedgerRecord } from "../../../backend/ledgers";
 const v1 = require("../../../backend/lumens");
 const v2v3 = require("../../../backend/v2v3/lumens");
+
+const REDIS_LEDGER_KEY_TEST = "ledgers_test";
+const REDIS_PAGING_TOKEN_KEY_TEST = "paging_token_test";
 
 // 10s timeout added for the multiple calls to Horizon per test, which occasionally
 // surpasses the default 2s timeout causing an error.
@@ -123,6 +127,153 @@ describe("lumens v3", function () {
       for (var k in obj) {
         chai.expect(obj[k].toString()).to.not.be.empty;
       }
+    });
+  });
+});
+
+describe("ledgers", function () {
+  describe("updateCache", function () {
+    it("should store cache with correct data", async function () {
+      // cleanup
+      await redisClient.del(REDIS_LEDGER_KEY_TEST);
+      await redisClient.del(REDIS_PAGING_TOKEN_KEY_TEST);
+
+      const ledgers: LedgerRecord = [
+        {
+          paging_token: "101",
+          sequence: 10001,
+          successful_transaction_count: 10,
+          failed_transaction_count: 5,
+          operation_count: 50,
+          closed_at: "2022-01-11T01:06:00Z",
+        },
+        {
+          paging_token: "102",
+          sequence: 10002,
+          successful_transaction_count: 20,
+          failed_transaction_count: 10,
+          operation_count: 100,
+          closed_at: "2022-01-12T01:06:00Z",
+        },
+        {
+          paging_token: "103",
+          sequence: 10003,
+          successful_transaction_count: 30,
+          failed_transaction_count: 20,
+          operation_count: 200,
+          closed_at: "2022-01-12T01:07:00Z",
+        },
+      ];
+
+      await updateCache(
+        ledgers,
+        REDIS_LEDGER_KEY_TEST,
+        REDIS_PAGING_TOKEN_KEY_TEST,
+      );
+
+      const cachedLedgers = await redisClient.get(REDIS_LEDGER_KEY_TEST);
+      const cachedPagingToken = await redisClient.get(
+        REDIS_PAGING_TOKEN_KEY_TEST,
+      );
+      chai.expect(JSON.parse(cachedLedgers as string)).to.eql([
+        {
+          date: "01-12",
+          transaction_count: 80,
+          operation_count: 300,
+        },
+        {
+          date: "01-11",
+          transaction_count: 15,
+          operation_count: 50,
+        },
+      ]);
+      chai.assert.equal(cachedPagingToken as string, "103");
+    });
+  });
+  describe("catchup", function () {
+    this.timeout(20000);
+    it("should handle large amounts of ledgers", async function () {
+      // cleanup
+      await redisClient.del(REDIS_LEDGER_KEY_TEST);
+      await redisClient.del(REDIS_PAGING_TOKEN_KEY_TEST);
+
+      await catchup(
+        REDIS_LEDGER_KEY_TEST,
+        "168143176454897664",
+        REDIS_PAGING_TOKEN_KEY_TEST,
+        1000,
+      );
+
+      const cachedLedgers = await redisClient.get(REDIS_LEDGER_KEY_TEST);
+      const cachedPagingToken = await redisClient.get(
+        REDIS_PAGING_TOKEN_KEY_TEST,
+      );
+
+      chai
+        .expect(JSON.parse(cachedLedgers as string))
+        .to.eql([
+          { date: "01-12", transaction_count: 403018, operation_count: 781390 },
+        ]);
+      chai.assert.equal(cachedPagingToken as string, "168147471422193664");
+    });
+    it("should not update if caught up", async function () {
+      await redisClient.set(REDIS_LEDGER_KEY_TEST, "[]");
+      await redisClient.set(REDIS_PAGING_TOKEN_KEY_TEST, "10");
+
+      await catchup(
+        REDIS_LEDGER_KEY_TEST,
+        "now",
+        REDIS_PAGING_TOKEN_KEY_TEST,
+        0,
+      );
+
+      const cachedLedgers = await redisClient.get(REDIS_LEDGER_KEY_TEST);
+      const cachedPagingToken = await redisClient.get(
+        REDIS_PAGING_TOKEN_KEY_TEST,
+      );
+      chai.assert.equal(cachedLedgers as string, "[]");
+      chai.assert.equal(cachedPagingToken as string, "10");
+    });
+    it("should only store last 30 days", async function () {
+      // cleanup
+      await redisClient.del(REDIS_LEDGER_KEY_TEST);
+      await redisClient.del(REDIS_PAGING_TOKEN_KEY_TEST);
+
+      const ledgers: LedgerRecord = [];
+      for (let i = 1; i <= 31; i++) {
+        ledgers.push(
+          {
+            paging_token: String(100 + 2 * i - 1),
+            sequence: 1000 + 2 * i - 1,
+            successful_transaction_count: 10 + i,
+            failed_transaction_count: 5 + i,
+            operation_count: 50 + i,
+            closed_at: `2022-01-${("0" + i).slice(-2)}T01:06:00Z`,
+          },
+          {
+            paging_token: String(101 + 2 * i),
+            sequence: 1001 + 2 * i,
+            successful_transaction_count: 10 + i,
+            failed_transaction_count: 5 + i,
+            operation_count: 50 + i,
+            closed_at: `2022-01-${("0" + i).slice(-2)}T01:06:01Z`,
+          },
+        );
+      }
+      await updateCache(
+        ledgers,
+        REDIS_LEDGER_KEY_TEST,
+        REDIS_PAGING_TOKEN_KEY_TEST,
+      );
+
+      const cachedLedgers = await redisClient.get(REDIS_LEDGER_KEY_TEST);
+
+      const cachedPagingToken = await redisClient.get(
+        REDIS_PAGING_TOKEN_KEY_TEST,
+      );
+      chai.assert.equal(JSON.parse(cachedLedgers as string).length, 30);
+      chai.assert.equal(JSON.parse(cachedLedgers as string)[0].date, "01-31");
+      chai.assert.equal(cachedPagingToken as string, "163");
     });
   });
 });
