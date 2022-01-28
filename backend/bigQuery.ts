3dad4cbe06f1c29bfa433645b57afb89b99e673b
Alec Charbonneau <aleccharb21@gmail.com>
2022-01-28 09:55:25 -0800

    use gcloud/ only for file

--- backend/bigQuery.ts
+++ backend/bigQuery.ts
@@ -1,7 +1,7 @@
 import { BigQuery } from "@google-cloud/bigquery";
 
 const options = {
-  keyFilename: "gcloud/credentials/service-account.json",
+  keyFilename: "gcloud/service-account.json",
   projectId: "hubble-261722",
 };
 
