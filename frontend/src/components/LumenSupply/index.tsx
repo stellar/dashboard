016392458cee4a77f8efc81393c3e2f059794a34
Iveta <quietbits@gmail.com>
2022-03-02 14:40:45 -0500

    v2: Lumen supply

--- frontend/src/components/LumenSupply/index.tsx
+++ frontend/src/components/LumenSupply/index.tsx
@@ -1,30 +1,110 @@
 import { useEffect } from "react";
+import { TextLink, Icon } from "@stellar/design-system";
+import { useDispatch } from "react-redux";
+import BigNumber from "bignumber.js";
+
 import { SectionCard } from "components/SectionCard";
+import { DASHBOARD_URL, FRIENDBOT_PUBLIC_ADDRESS } from "constants/settings";
+import { fetchLumenSupplyAction } from "ducks/lumenSupply";
+import { formatAmount } from "helpers/formatAmount";
+import { useRedux } from "hooks/useRedux";
+import { ActionStatus, Network } from "types";
 
 import "./styles.scss";
 
-export const LumenSupply = () => {
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("/api/v2/lumens");
-        const data = await response.json();
-        console.log("api response: ", data);
-      } catch (e) {
-        // do nothing
-      }
-    };
+export const LumenSupply = ({
+  network = Network.MAINNET,
+}: {
+  network?: Network;
+}) => {
+  const { lumenSupply } = useRedux("lumenSupply");
+  const { data } = lumenSupply;
+  const dispatch = useDispatch();
+  const isTestnet = network === Network.TESTNET;
+  const apiEndpoint = `${DASHBOARD_URL}/api/v3/lumens/`;
 
-    fetchData();
-  }, []);
+  useEffect(() => {
+    dispatch(fetchLumenSupplyAction(network));
+  }, [network, dispatch]);
+
+  const items = [
+    {
+      id: "circulating",
+      label: isTestnet ? (
+        <>
+          Friendbot:
+          <TextLink
+            href={`https://horizon-testnet.stellar.org/accounts/${FRIENDBOT_PUBLIC_ADDRESS}`}
+          >
+            GAIH
+          </TextLink>
+        </>
+      ) : (
+        "Circulating Supply"
+      ),
+      amount: data?.circulating,
+      apiUrl: isTestnet ? null : apiEndpoint,
+    },
+    {
+      id: "nonCirculating",
+      label: "Non-Circulating Supply",
+      amount: data?.nonCirculating,
+      apiUrl: isTestnet ? null : apiEndpoint,
+    },
+    {
+      id: "total",
+      label: "Total XLM Supply",
+      amount: data?.total,
+      apiUrl: isTestnet ? null : apiEndpoint,
+    },
+  ];
 
   return (
     <SectionCard
       title="Total XLM Issued"
       titleLinkLabel="Lumen Supply Metrics"
       titleLink="https://developers.stellar.org/docs/glossary/lumen-supply/"
+      isLoading={lumenSupply.status === ActionStatus.PENDING}
+      noData={!data}
     >
-      Total XLM issued content
+      {data ? (
+        <div className="LumenSupply">
+          <div className="LumenSupply__chart">
+            <div className="LumenSupply__chart__item" />
+          </div>
+          <div className="LumenSupply__supply">
+            {items.map((i) => (
+              <div key={i.id} className="LumenSupply__supply__item">
+                <div className="LumenSupply__supply__item__info">
+                  <div
+                    className={`LumenSupply__supply__item__label
+                      ${
+                        i.id === "total"
+                          ? "LumenSupply__supply__item__bold"
+                          : ""
+                      }
+                    `}
+                  >
+                    <div className="LumenSupply__supply__item__chart" />
+                    {i.label}
+                  </div>
+                  {i.apiUrl ? (
+                    <TextLink href={i.apiUrl} iconRight={<Icon.ExternalLink />}>
+                      API
+                    </TextLink>
+                  ) : null}
+                </div>
+                <div className="LumenSupply__supply__item__amounts">
+                  <div className="LumenSupply__supply__item__bold">
+                    {formatAmount(i.amount || 0)} XLM
+                  </div>
+                  <div>{new BigNumber(i.amount || 0).toFormat(7)} XLM</div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      ) : null}
     </SectionCard>
   );
 };
--- frontend/src/pages/Testnet.tsx
+++ frontend/src/pages/Testnet.tsx
@@ -1,6 +1,7 @@
 import { NetworkStatus } from "components/NetworkStatus";
 import { LastLedger } from "components/LastLedger";
 import { LastLedgersInfo } from "components/LastLedgersInfo";
+import { LumenSupply } from "components/LumenSupply";
 import { NetworkNodes } from "components/NetworkNodes";
 import { Network } from "types";
 
@@ -9,6 +10,7 @@ export const Testnet = () => (
     <NetworkStatus network={Network.TESTNET} />
     <LastLedger network={Network.TESTNET} />
     <LastLedgersInfo network={Network.TESTNET} />
+    <LumenSupply network={Network.TESTNET} />
     <NetworkNodes network={Network.TESTNET} />
   </div>
 );
