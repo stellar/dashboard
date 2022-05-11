d59fd01683636e70c9c90987bf5c8b79b5f601de
italomlp <italomlp@gmail.com>
2022-05-11 12:03:15 -0300

    feat(v2): add ledger info component to testnet

--- frontend/src/pages/Testnet.tsx
+++ frontend/src/pages/Testnet.tsx
@@ -1,6 +1,7 @@
 import { NetworkStatus } from "components/NetworkStatus";
 import { LastLedger } from "components/LastLedger";
 import { LastLedgersInfo } from "components/LastLedgersInfo";
+import { LedgerInfo } from "components/LedgerInfo";
 import { LumenSupply } from "components/LumenSupply";
 import { NetworkNodes } from "components/NetworkNodes";
 import { Network } from "types";
@@ -10,6 +11,7 @@ export const Testnet = () => (
     <NetworkStatus network={Network.TESTNET} />
     <LastLedger network={Network.TESTNET} />
     <LastLedgersInfo network={Network.TESTNET} />
+    <LedgerInfo network={Network.TESTNET} />
     <LumenSupply network={Network.TESTNET} />
     <NetworkNodes network={Network.TESTNET} />
   </div>
