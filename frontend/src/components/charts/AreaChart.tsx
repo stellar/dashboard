602ec939846539804f5d81978d6ae80863e2bb52
italomlp <italomlp@gmail.com>
2022-06-08 16:43:55 -0300

    feat(v2.1): add AreaChart based on two values

---
+++ frontend/src/components/charts/AreaChart.tsx
@@ -0,0 +1,60 @@
+import { Area } from "recharts";
+
+import { BaseTwoValuesChart } from "./BaseTwoValuesChart";
+import { BaseTwoValuesChartProps } from "./BaseTwoValuesChart/types";
+
+type AreaChartProps = Omit<
+  BaseTwoValuesChartProps,
+  "xContentPaddingEnabled"
+> & {
+  /**
+   * Name/Label of the primary value, to be showed on the Legend. If not provided,
+   * it will show `primaryValue`.
+   */
+  primaryValueName?: string;
+  /**
+   * Name/Label of the secondary value, to be showed on the Legend. If not provided,
+   * it will show `secondaryValue`.
+   */
+  secondaryValueName?: string;
+};
+
+/**
+ * Area Chart component. It is used to display a chart with a series-based data.
+ *
+ * @see {@link BaseTwoValuesChart} for more details on how this data and props work.
+ */
+export const AreaChart = ({
+  primaryValueName,
+  secondaryValueName,
+  primaryValueOnly = false,
+  ...props
+}: AreaChartProps) => {
+  return (
+    <BaseTwoValuesChart primaryValueOnly={primaryValueOnly} {...props}>
+      <Area
+        type="monotone"
+        dataKey="primaryValue"
+        stroke="var(--pal-graph-primary)"
+        fill="var(--pal-graph-primary)"
+        name={primaryValueName}
+        strokeWidth={2}
+      />
+
+      {!primaryValueOnly && (
+        <Area
+          type="monotone"
+          dataKey="secondaryValue"
+          stroke="var(--pal-graph-secondary)"
+          fill="var(--pal-graph-secondary)"
+          name={secondaryValueName}
+          strokeWidth={2}
+        />
+      )}
+    </BaseTwoValuesChart>
+  );
+};
+
+AreaChart.TimeRange = BaseTwoValuesChart.TimeRange;
+
+export const TimeRange = AreaChart.TimeRange;
