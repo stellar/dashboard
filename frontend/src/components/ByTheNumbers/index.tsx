7371c37d5d570b82efab6bf2739143645be52c4d
Italo Menezes <italomlp@gmail.com>
2022-06-10 16:11:50 -0300

    SDFD-6 Feat: By the numbers section (#268)
    * feat(v2): add amount info card component
    
    * feat(v2): add dex card component
    
    * feat(v2): update by the numbers' section UI
    
    * feat(v2): add dex integration for by the numbers section
    
    * feat(v2): update dex data integration
    
    * fix(v2): webpack proxy config
    
    * feat(v2): dex fluctuation format to 2 decimal places
    
    * refactor(v2): apply CR suggestions

--- frontend/src/components/ByTheNumbers/index.tsx
+++ frontend/src/components/ByTheNumbers/index.tsx
@@ -1,14 +1,82 @@
+import { useEffect, useMemo } from "react";
+import { Icon } from "@stellar/design-system";
+import BigNumber from "bignumber.js";
+import { useDispatch } from "react-redux";
+
 import { SectionCard } from "components/SectionCard";
+import { DexCard } from "components/DexCard";
+import { AmountInfoCard } from "components/AmountInfoCard";
+
+import { useRedux } from "hooks/useRedux";
+import { fetchDexDataAction } from "ducks/dex";
+import { ActionStatus } from "types";
 
 import "./styles.scss";
 
-export const ByTheNumbers = () => (
-  <SectionCard
-    title="By the Numbers"
-    titleLinkLabel="More stats"
-    // TODO: update link
-    titleLink="https://horizon.stellar.org/operations?order=desc&limit=20"
-  >
-    By the numbers content
-  </SectionCard>
-);
+export const ByTheNumbers = () => {
+  const { dex } = useRedux("dex");
+  const { data } = dex;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchDexDataAction());
+  }, [dispatch]);
+
+  const dexData = useMemo(() => {
+    if (!data) {
+      return null;
+    }
+
+    return {
+      dailyActiveAccounts: new BigNumber(data.dailyActiveAccounts).toFormat(),
+      payments24HRs: new BigNumber(data.payments24HRs).toFormat(),
+      totalUniqueAssets: new BigNumber(data.totalUniqueAssets).toFormat(),
+      trades: {
+        fluctuation: data.trades.fluctuation,
+        overall: new BigNumber(data.trades.overall).toFormat(),
+        last24HR: new BigNumber(data.trades.last24HR).toFormat(),
+      },
+    };
+  }, [data]);
+
+  return (
+    <SectionCard
+      title="By the Numbers"
+      titleLinkLabel="API"
+      titleLink="/api/dex/all"
+      isLoading={dex.status === ActionStatus.PENDING}
+      noData={!dexData}
+    >
+      {dexData && (
+        <>
+          <div className="ByTheNumbers__dex_cards_container">
+            <DexCard
+              title="DEX TRADES"
+              icon={<Icon.RefreshCcw width={16} height={16} />}
+              last24HRValue={dexData.trades.last24HR}
+              overallValue={dexData.trades.overall}
+              fluctuation={dexData.trades.fluctuation}
+            />
+          </div>
+          <div className="ByTheNumbers__amount_cards_container">
+            <AmountInfoCard
+              title="Total Unique Assets"
+              titleIcon={<Icon.Database />}
+              amount={dexData.totalUniqueAssets}
+            />
+            <AmountInfoCard
+              title="Daily active accounts"
+              titleIcon={<Icon.User />}
+              amount={dexData.dailyActiveAccounts}
+            />
+            <AmountInfoCard
+              title="24HR Payments"
+              titleIcon={<Icon.ChevronsRight />}
+              amount={dexData.payments24HRs}
+            />
+          </div>
+        </>
+      )}
+    </SectionCard>
+  );
+};
---
+++ frontend/src/ducks/dex.ts
@@ -0,0 +1,72 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import BigNumber from "bignumber.js";
+
+import { RootState } from "config/store";
+
+import { getErrorString } from "helpers/getErrorString";
+
+import {
+  ActionStatus,
+  RejectMessage,
+  DexDataInitialState,
+  DexData,
+} from "types";
+
+export const fetchDexDataAction = createAsyncThunk<
+  DexData,
+  void,
+  { rejectValue: RejectMessage; state: RootState }
+>("dex/fetchDexDataAction", async (_, { rejectWithValue }) => {
+  try {
+    const response = await fetch("api/dex/all");
+    const { tradesLast24H, paymentsLast24h, uniqueAssets, activeAccounts } =
+      await response.json();
+
+    return {
+      trades: {
+        fluctuation: new BigNumber(tradesLast24H.change || 0).toNumber(),
+        last24HR: tradesLast24H.trades_last_24h,
+        overall: tradesLast24H.overall,
+      },
+      payments24HRs: paymentsLast24h,
+      totalUniqueAssets: uniqueAssets,
+      dailyActiveAccounts: activeAccounts,
+    };
+  } catch (error) {
+    return rejectWithValue({
+      errorString: getErrorString(error),
+    });
+  }
+});
+
+const initialState: DexDataInitialState = {
+  data: null,
+  status: undefined,
+  errorString: undefined,
+};
+
+const dexDataSlice = createSlice({
+  name: "dex",
+  initialState,
+  reducers: {
+    resetDexDataAction: () => initialState,
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchDexDataAction.pending, (state = initialState) => {
+      state.status = ActionStatus.PENDING;
+    });
+    builder.addCase(fetchDexDataAction.fulfilled, (state, action) => {
+      state.data = action.payload;
+      state.status = ActionStatus.SUCCESS;
+    });
+    builder.addCase(fetchDexDataAction.rejected, (state, action) => {
+      state.status = ActionStatus.ERROR;
+      state.errorString = action.payload?.errorString;
+    });
+  },
+});
+
+export const dexDataSelector = (state: RootState) => state.dex;
+
+export const { reducer } = dexDataSlice;
+export const { resetDexDataAction } = dexDataSlice.actions;
