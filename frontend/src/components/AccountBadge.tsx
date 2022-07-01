e7d10d4c503f998f4db9ba4d0ba5d1c920fb4592
Lucas Coronel <lccoronel7@icloud.com>
2022-07-01 18:03:48 -0300

    feat: Account Badge component

---
+++ frontend/src/components/AccountBadge.tsx
@@ -0,0 +1,61 @@
+import { knownAccounts } from "constants/knownAccounts";
+import { useState } from "react";
+
+interface AccountBadgeProps {
+  horizonURL: string;
+  id: string;
+  account?: string;
+}
+
+interface ObjectAccount {
+  url: string;
+  name: string;
+  icon: string;
+}
+
+export const AccountBadge: React.FC<AccountBadgeProps> = ({
+  horizonURL,
+  id,
+  account,
+}) => {
+  const [isObjectAccount, setIsObjectAccount] = useState({} as ObjectAccount);
+
+  const convertedId = id as unknown as keyof typeof knownAccounts;
+
+  if (
+    knownAccounts[convertedId] ===
+    "GDCIUCGL7VEMMF6VYJOW75KQ5ZCLHAQBRM6EPFTKCRWUYVUOOYQCKC5A"
+  ) {
+    setIsObjectAccount(knownAccounts[convertedId] as ObjectAccount);
+  }
+
+  return (
+    <span className="account">
+      <code>
+        <a
+          className="RecentOperations__link"
+          href={horizonURL + "/accounts/" + id}
+          target="_blank"
+          rel="noreferrer"
+        >
+          {id.substring(0, 4)}
+        </a>
+      </code>
+
+      {knownAccounts[convertedId] &&
+        id !== account &&
+        (isObjectAccount ? (
+          <a
+            href={isObjectAccount.url}
+            target="_blank"
+            title={isObjectAccount.name}
+            rel="noreferrer"
+          >
+            <img src={isObjectAccount.icon} alt="" />
+          </a>
+        ) : (
+          <span className="account-tag">{knownAccounts[convertedId]}</span>
+        ))}
+    </span>
+  );
+};
