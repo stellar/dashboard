12098c95715173f274e15469189c59c23fb8cf3c
Miguel Nieto <39246879+miguelnietoa@users.noreply.github.com>
2026-07-08 19:46:14 -0500

    Add brand design tokens, base styles, and Card component

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./components/App.jsx";
 
+// Self-hosted brand fonts (Stellar Brand Guidelines 2026)
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
+import "@fontsource/lora/500.css";
+import "@fontsource/lora/600.css";
+
 // Add SCSS import
 import "./scss/index.scss";
 
